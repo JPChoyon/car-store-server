e3bd4a4d274513fca36b61bf4b0315c2ad251769
JPChoyon <kotjpckhan@gmail.com>
2025-02-14 02:23:00 +0600

    authonication added

--- src/app/modules/order/order.router.ts
+++ src/app/modules/order/order.router.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { orderController } from './order.controller';
-import auth from '../../middleware/auth';
 
 const orderRouter = Router();
 
-orderRouter.get('/', auth('user'), orderController.findAllOrder);
+orderRouter.get('/', orderController.findAllOrder);
 orderRouter.post('/', orderController.createOrderInDB);
 orderRouter.get('/revenue', orderController.calculateRevenue);
 orderRouter.get('/:orderId', orderController.findAOrder);
